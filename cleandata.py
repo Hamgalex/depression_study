fcc608889bc05d092dff8a86838256a99bd4d09e
Héctor Alejandro Márquez González <hamgalex@hotmail.com>
2023-02-19 16:55:53 -0600

    se lee el dataset

--- cleandata.py
+++ cleandata.py
@@ -3,3 +3,6 @@ import numpy as np
 import fuzzywuzzy
 from fuzzywuzzy import process
 import charset_normalizer
+
+depression_study = pd.read_csv("datasets/depression_study.1000copy.csv",encoding='utf-8')
+print(depression_study)
