9a61368a2d0eff28cd79d00121dee57a0848711b
Héctor Alejandro Márquez González <hamgalex@hotmail.com>
2023-03-02 10:00:12 -0600

    se creo el excel con las medicinas anteriores y se lee

--- manejodatos/matrizmedicinas_delta.py
+++ manejodatos/matrizmedicinas_delta.py
@@ -1,7 +1,10 @@
 import pandas as pd
 import numpy as np
 
-depression_study = pd.read_csv("datasets/depression_study.10000.csv",encoding='utf-8')
-np.random.seed(0)
+recetas = pd.read_csv("datasets/recetas.csv",encoding='utf-8')
 
-recetas = depression_study[~depression_study["medication"].isna()]
+print(recetas.head())
+
+
+
+
