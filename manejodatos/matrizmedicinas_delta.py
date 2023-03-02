9af393cb98480468901828b2d3a3228d723ea5bf
Héctor Alejandro Márquez González <hamgalex@hotmail.com>
2023-03-02 11:07:48 -0600

    se pasaron todas las combinaciones de medicinas

--- manejodatos/matrizmedicinas_delta.py
+++ manejodatos/matrizmedicinas_delta.py
@@ -6,5 +6,11 @@ recetas = pd.read_csv("datasets/recetas.csv",encoding='utf-8')
 print(recetas.head())
 
 
+medicinas=["cymbalta","savella","lexapro","effexor","wellbutrin","remeron","paxil","zoloft","celexa","prozac","none"]
 
 
+
+
+for medicina_anterior in medicinas:
+    for medicina_actual in medicinas:
+        print(medicina_anterior+medicina_actual)
