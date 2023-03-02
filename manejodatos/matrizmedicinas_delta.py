ee0314aa35239fde48ad67333f2306d2178a9875
Héctor Alejandro Márquez González <hamgalex@hotmail.com>
2023-03-02 11:25:21 -0600

    se hizo el programa para sacar la matriz de medicinas

--- manejodatos/matrizmedicinas_delta.py
+++ manejodatos/matrizmedicinas_delta.py
@@ -3,14 +3,22 @@ import numpy as np
 
 recetas = pd.read_csv("datasets/recetas.csv",encoding='utf-8')
 
-print(recetas.head())
+#print(recetas.head())
 
 
 medicinas=["cymbalta","savella","lexapro","effexor","wellbutrin","remeron","paxil","zoloft","celexa","prozac","none"]
 
-
+df = pd.DataFrame(
+    columns=medicinas,
+    index=medicinas)
 
 
 for medicina_anterior in medicinas:
     for medicina_actual in medicinas:
-        print(medicina_anterior+medicina_actual)
+        combinacion=recetas[recetas['medication_anterior']==medicina_anterior]
+        combinacion=combinacion[recetas['medication']==medicina_actual]
+        promedio=combinacion['delta'].mean()
+        df[medicina_actual][medicina_anterior]=promedio
+
+
+df.to_csv('output.csv')
